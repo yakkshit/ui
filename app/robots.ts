7b50337eb47a0d22e115821056526fde070ca836
yakkshit <saiyakkshit2001@gmail.com>
2024-07-30 12:16:23 +0530

    fix

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,22 @@ import { MetadataRoute } from 'next'
  
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
+    rules: [
+      {
+        userAgent: 'Googlebot',
+        allow: ['/'],
+        disallow: '/private/',
+      },
+      {
+        userAgent: ['Applebot', 'Bingbot'],
+        disallow: ['/'],
+      },
+      {
       userAgent: '*',
       allow: '/',
       disallow: '/private/',
-    },
+      },
+    ],
     sitemap: 'https://ui.cedzlabs.com/sitemap.xml',
   }
 }
