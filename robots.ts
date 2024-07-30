a85596d4f807b37db478b9e50f0cf129c1269d86
yakkshit <saiyakkshit2001@gmail.com>
2024-07-30 15:38:21 +0530

    fix

--- robots.ts
+++ robots.ts
@@ -10,7 +10,7 @@ export default function robots(): MetadataRoute.Robots {
       },
       {
         userAgent: ['Applebot', 'Bingbot'],
-        disallow: ['/'],
+        allow: ['/'],
       },
       {
         userAgent: '*',
