6750c771880f0a532751c6677f52aafd9ceb1224
yakkshit <saiyakkshit2001@gmail.com>
2024-09-02 11:31:51 +0530

    minor fix

--- app/(docs)/yakkshit/page.tsx
+++ app/(docs)/yakkshit/page.tsx
@@ -1,11 +1,12 @@
 import YakkshitResume from "@/app/(docs)/yakkshit/components/yakkshit-resume";
 import resumeData from "@/app/(docs)/yakkshit/data/resumeData.json";
 
+const data = resumeData;
 
 const Yakkshit: React.FC = () => {
     return (
         <>
-         <YakkshitResume resumeData={resumeData} />
+         <YakkshitResume resumeData={data} />
         </>
 
     );
